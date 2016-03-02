a12b714dca466fd38fc1ff8e2bc2626c73de22c8
lifenglifeng001 <lifenglifeng001@gmail.com>
2016-03-02 18:46:26 +0800

    change readme

--- __openerp__.py
+++ __openerp__.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "tree_wid",
+    'name': "many2one_tree",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        odoo tree widget for many2one field
+        """,
 
     'description': """
-        Long description of module's purpose
+        odoo tree widget for many2one field
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Li Feng",
+    'website': "https://github.com/lifenglifeng001/many2one_tree",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Tools',
+    'version': '8.0',
+    'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
